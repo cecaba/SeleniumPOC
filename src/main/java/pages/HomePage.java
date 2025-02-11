b93a9dc133ae9aa239f9159837c30fe209f04909
Svetlana Baus Torbica <cecabaiconia@gmail.com>
2025-02-11 09:54:21 +0100

    SeleniumPOC change xpath to css

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -3,7 +3,7 @@ package pages;
 import org.openqa.selenium.By;
 
 public class HomePage extends BasePage {
-    private final By homePageTitle = By.xpath("//head/title");
+    private final By homePageTitle = By.cssSelector("head > title");
 
     public String getHomePageTitle(){
         waitForElementPresence(homePageTitle);
